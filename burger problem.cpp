65385229737bde484664908ec4e8b777bde8608a
CHESTERKING4204 <114911683+CHESTERKING4204@users.noreply.github.com>
2022-10-05 00:01:58 +0530

    burger problem codechef solution
    hacktoberfest_2022_3rd

---
+++ burger problem.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+#include<climits>
+#include<math.h>
+int main(){
+ int n;
+ cin>>n;
+ 
+int arr[n],a[n];
+for(int i=0;i<n;i++){
+	cin>>arr[i]>>a[i];
+}
+    for(int i=0;i<n;i++){
+    	cout<<min(arr[i],a[i]) <<  endl;
+    	
+	}
+}
+
